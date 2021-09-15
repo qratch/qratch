cf22e28b131c2bbfdc1e89ef10f0ab3e6966b318
hota1024 <hotalog@hota1024.com>
2021-09-15 13:09:18 +0900

    🏷️ add Cursor interface

---
+++ src/Cursor/Cursor.ts
@@ -0,0 +1,34 @@
+import { Pointable } from '@/Pointable/Pointable'
+import { CursorStyle } from './CursorStyle'
+
+/**
+ * Cursor interface.
+ */
+export interface Cursor {
+  /**
+   * x position.
+   */
+  readonly x: number
+
+  /**
+   * y position.
+   */
+  readonly y: number
+
+  /**
+   * returns the amount the cursor has moved since one frame ago.
+   */
+  delta(): Pointable
+
+  /**
+   * set cursor style.
+   *
+   * @param style style.
+   */
+  setStyle(style: CursorStyle): this
+
+  /**
+   * returns cursor style.
+   */
+  getStyle(): CursorStyle
+}
