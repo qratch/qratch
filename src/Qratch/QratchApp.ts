3263a8ad1a1bce7ac3cc778783fd1881e83a49a0
hota1024 <hotalog@hota1024.com>
2021-09-18 08:00:44 +0900

    🏷️ add QratchApp abstract class

---
+++ src/Qratch/QratchApp.ts
@@ -0,0 +1,14 @@
+/**
+ * QratchApp abstract class.
+ */
+export abstract class QratchApp {
+  /**
+   * update.
+   */
+  protected abstract update(): void
+
+  /**
+   * draw.
+   */
+  protected abstract draw(): void
+}
