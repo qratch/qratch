6f0861d27990fd98901b8265f6e53032ec9f94c2
hota1024 <hotalog@hota1024.com>
2021-09-18 07:24:59 +0900

    ✨ add RequestAnimationFrameTicker class

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -4,6 +4,7 @@ import { HTMLElementKeyboard } from './HTMLElementKeyboard/HTMLElementKeyboard'
 import { HTMLElementMouse } from './HTMLElementMouse/HTMLElementMouse'
 import { Drawer } from './Drawer/Drawer'
 import { Vec2 } from './Vec2/Vec2'
+import { RequestAnimationFrameTicker } from './RequestAnimationFrameTicker/RequestAnimationFrameTicker'
 
 const canvas = document.getElementById('canvas')
 
@@ -16,13 +17,11 @@ const c = new Drawer(r)
 const keyboard = new HTMLElementKeyboard(canvas)
 const cursor = new HTMLElementCursor(canvas)
 const mouse = new HTMLElementMouse(canvas)
-let count = 0
 let fpsCount = 0
 let fps = 0
 const pos = new Vec2(0, 0)
 
 const frame = () => {
-  count++
   fpsCount++
 
   if (keyboard.pressed('ArrowLeft')) {
@@ -37,12 +36,12 @@ const frame = () => {
     pos.y -= 1
   }
 
-  if (keyboard.pressed('Space')) {
+  if (keyboard.pressed('ArrowDown')) {
     pos.y += 1
   }
 
   c.fillRect(0, 0, 480, 480, 'white')
-  c.fillText(`${count}:${fps}`, 8, 16, 'black')
+  c.fillText(`${ticker.frames()}:${fps}`, 8, 16, 'black')
 
   pos.add(mouse.wheel() / 100)
 
@@ -51,10 +50,11 @@ const frame = () => {
   keyboard.onFrameEnd()
   cursor.onFrameEnd()
   mouse.onFrameEnd()
-  requestAnimationFrame(frame)
 }
 
-frame()
+const ticker = new RequestAnimationFrameTicker()
+ticker.addCallback(frame)
+ticker.start()
 
 setInterval(() => {
   fps = fpsCount
