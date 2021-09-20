19d2e3b2fb3cbf475083e7ea688b69ba1899a31b
hota1024 <hotalog@hota1024.com>
2021-09-21 01:16:04 +0900

    🏷️ add install, init and frameTickerStart hooks

--- src/Component/Component.ts
+++ src/Component/Component.ts
@@ -3,12 +3,27 @@
  */
 export interface Component {
   /**
+   * call on component installed.
+   */
+  onInstall?(): void
+
+  /**
+   * call on application initialization.
+   */
+  onInit?(): void
+
+  /**
+   * call on frame ticker started.
+   */
+  onFrameTickerStart?(): void
+
+  /**
    * call on frame start.
    */
-  onFrameStart?: () => void
+  onFrameStart?(): void
 
   /**
    * call on frame end.
    */
-  onFrameEnd?: () => void
+  onFrameEnd?(): void
 }
