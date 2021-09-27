a0c74ca5174793c335aa092fa6cc277c3f5fb5b3
hota1024 <hotalog@hota1024.com>
2021-09-27 09:34:44 +0900

    :fire: remove tests

--- tests/Vec2/Vec2.test.ts
+++
@@ -1,9 +0,0 @@
-import { Vec2 } from '@/Vec2/Vec2'
-
-describe('Vec2 test', () => {
-  test('Vec2#constructor', () => {
-    expect(new Vec2(10, 20)).toMatchObject({ x: 10, y: 20 })
-    expect(new Vec2(30)).toMatchObject({ x: 30, y: 30 })
-    expect(new Vec2({ x: 1, y: 2 })).toMatchObject({ x: 1, y: 2 })
-  })
-})
