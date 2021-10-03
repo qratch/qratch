a126b787e5badd162158607d94d6a47d490b1516
hota1024 <hotalog@hota1024.com>
2021-10-04 06:24:28 +0900

    🔧 update rollup config

--- packages/qratch/rollup.config.js
+++ packages/qratch/rollup.config.js
@@ -1,9 +1,8 @@
 import ts from '@wessberg/rollup-plugin-ts'
 import eslint from '@rollup/plugin-eslint'
 import { terser } from 'rollup-plugin-terser'
-import visualizer from 'rollup-plugin-visualizer'
-import pkg from './package.json'
 import { builtinModules } from 'module'
+import pkg from './package.json'
 
 export default [
   {
@@ -11,49 +10,13 @@ export default [
     output: [
       {
         file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-      {
-        file: pkg.umd,
         format: 'umd',
         name: 'Qratch',
         moduleName: 'Qratch',
         sourcemap: true,
-        plugins: [
-          visualizer({
-            filename: 'dist/umd/stats.html',
-          }),
-        ],
       },
     ],
     plugins: [eslint(), ts(), terser()],
-    external: [
-      ...builtinModules,
-      ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
-      ...(pkg.devDependencies == null ? [] : Object.keys(pkg.devDependencies)),
-      ...(pkg.peerDependencies == null
-        ? []
-        : Object.keys(pkg.peerDependencies)),
-    ],
-  },
-  {
-    input: 'src/sandbox.ts',
-    output: [
-      {
-        file: 'dist/umd/sandbox.js',
-        format: 'umd',
-        name: 'Qratch',
-        moduleName: 'Qratch',
-        sourcemap: true,
-      },
-    ],
-    plugins: [eslint(), ts()],
     external: [
       ...builtinModules,
       ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
