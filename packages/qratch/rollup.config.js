0b2792303e01955b014bb74fff6948c0317b4f32
hota1024 <hotalog@hota1024.com>
2021-10-04 06:42:02 +0900

    🔧 add createModuleConfig build utility

--- packages/qratch/rollup.config.js
+++ packages/qratch/rollup.config.js
@@ -1,29 +1,4 @@
-import ts from '@wessberg/rollup-plugin-ts'
-import eslint from '@rollup/plugin-eslint'
-import { terser } from 'rollup-plugin-terser'
-import { builtinModules } from 'module'
+import { createModuleConfig } from '../../build-utils/createModuleConfig'
 import pkg from './package.json'
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'umd',
-        name: 'Qratch',
-        moduleName: 'Qratch',
-        sourcemap: true,
-      },
-    ],
-    plugins: [eslint(), ts(), terser()],
-    external: [
-      ...builtinModules,
-      ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
-      ...(pkg.devDependencies == null ? [] : Object.keys(pkg.devDependencies)),
-      ...(pkg.peerDependencies == null
-        ? []
-        : Object.keys(pkg.peerDependencies)),
-    ],
-  },
-]
+export default createModuleConfig(pkg, 'qratch')
