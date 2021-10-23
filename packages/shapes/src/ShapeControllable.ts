b5a9837818ad84f58d1fd918b657f564e976a207
hota1024 <hotalog@hota1024.com>
2021-10-24 07:17:02 +0900

    🏷️ add base interfaces of shapes

---
+++ packages/shapes/src/ShapeControllable.ts
@@ -0,0 +1,5 @@
+/**
+ * ShapeControllable interface.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ShapeControllable {}
---
+++ packages/shapes/src/Shapeable.ts
@@ -0,0 +1,7 @@
+import { Copyable } from '@qratch/copyable'
+
+/**
+ * Shapeable interface.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Shapeable extends Copyable<Shapeable> {}
